25d333cab3be50d3462dee36f1c19cf200048d2e
Jisan Ahmed <jisanahmed10667@gmail.com>
2024-05-09 09:14:27 +0600

    Codechef Starter 133

---
+++ Codechef Starter 133/A/A.cpp
@@ -0,0 +1,111 @@
+//Bismillahir Rahman-ir Rahim
+#include<complex>
+#include<queue>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<chrono>
+#include<random>
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<map>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<stdlib.h>
+#include<iomanip>
+ 
+#define PI acos(-1)
+#define F first
+#define S second
+#define debug(x)  cout<<'>'<<#x<<":"<<x<<endl
+#define tara ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define Y printf("YES\n")
+#define N printf("NO\n")
+
+#define ll long long
+#define lll __int128
+#define ull unsigned long long
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define eb emplace_back
+#define pb push_back
+#define sc scanf
+#define pf printf
+#define endl '\n'
+#define INF 1<<30
+#define ll_INF 1LL<<62
+ 
+// const int M=1e5;
+
+int dx[] = {-1, 0, 0, 1};
+int dy[] = {0, -1, 1, 0};
+
+using namespace std ;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+
+void solve()
+{ 
+   ll n, i, j ;
+   cin>>n ;
+   string s ;
+   cin>>s ;
+   int a=-1, c=-1, cnt=0 ;
+   for(char x:s){
+    if(x=='a'){
+        a=cnt ;
+        break;
+    }
+    cnt++ ;
+   }
+   cnt=0 ;
+   for(char x:s){
+    if(x=='c'){
+        c=cnt ;
+        
+    }
+    cnt++ ;
+   }
+   cnt=0 ;
+   for(i=a+1; i<c ; i++){
+    if(s[i]=='b')cnt++ ;
+   }
+
+   cout<<cnt<<endl ;
+
+}
+ 
+
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+#endif
+    
+     tara ;
+    int t;
+    t = 1;
+    
+    cin >> t;
+
+    for(int i=0; i<t; i++) {
+        // if(i) printf("\n");
+        // cout << "Case " << i+1 << ": ";
+        solve();
+    }
+    return 0;
+}
